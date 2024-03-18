e4dabf4210fc16284ff6be1b96f84b3c9bc5c1f4
lukasmiljic <smiljicluka13@gmail.com>
2024-03-18 17:57:27 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Home from "./Pages/Home/Home";
+import Play from "./Pages/Play/Play";
 import LevelProvider from "./Providers/LevelProvider";
 import WordsPerMinuteProvider from "./Providers/WordsPerMinuteProvider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <LevelProvider>
         <WordsPerMinuteProvider>
-          <Home />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/Play" element={<Play />} />
+            </Routes>
+          </Router>
         </WordsPerMinuteProvider>
       </LevelProvider>
     </>
