a3474919af70df7aebaf9993fe674a6bcea296cd
lukasmiljic <smiljicluka13@gmail.com>
2024-03-19 17:46:52 +0100

    moved tracking of scrolling text into home :P

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,38 @@
 import { Stack } from "@mui/material";
-import Header from "../../Components/Header";
+import Header from "../../Components/Header/Header";
 import TypeWriter from "../../Components/TypeWriter/TypeWriter";
-import ScrollingTextProvider from "../../Providers/FadingTextProvider";
+import ScrollingTextProvider, {
+  useScrollingText,
+} from "../../Providers/FadingTextProvider";
+import { generateFadingText } from "../../Components/FadingText/FadingText";
+import { useRef, useEffect } from "react";
 
 const Home = () => {
+  const { scrollingTextEnabeled, updateScrollingTextEnabeled } =
+    useScrollingText();
+
+  useEffect(() => {
+    console.log("scrollingTextEnabeled " + scrollingTextEnabeled);
+  }, [scrollingTextEnabeled]);
+
+  let idRef = useRef();
+  useEffect(() => {
+    if (scrollingTextEnabeled) {
+      idRef.current = generateFadingText();
+      console.log("started " + idRef.current);
+    }
+
+    return () => {
+      if (idRef.current) {
+        console.log("stopping " + idRef.current);
+        clearInterval(idRef.current);
+      }
+    };
+  }, [scrollingTextEnabeled]);
+
   return (
     <Stack height={"100vh"}>
-      <ScrollingTextProvider>
-        <Header />
-      </ScrollingTextProvider>
+      <Header />
       <Stack
         direction="column"
         alignItems={"center"}
