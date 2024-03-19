3c5c8238515bf90c49792efab4a2045def513720
lukasmiljic <smiljicluka13@gmail.com>
2024-03-19 17:33:27 +0100

    added dialog component

---
+++ src/Components/Dialog/Dialog.jsx
@@ -0,0 +1,55 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+} from "@mui/material";
+import { useState, Link } from "react";
+
+const DialogComponent = (props) => {
+  const {
+    buttonTitle,
+    buttonVariant,
+    dialogTitle,
+    dialogContentText,
+    buttons,
+  } = props;
+
+  const [openDialog, setOpenDialog] = useState(false);
+
+  return (
+    <>
+      <Button variant={buttonVariant} onClick={() => setOpenDialog(true)}>
+        {buttonTitle}
+      </Button>
+
+      <Dialog
+        id={`${buttonTitle}-dialog`}
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}>
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{dialogContentText}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              onClick={() => setOpenDialog(false)}
+              {...(button.color && { color: button.color })}
+              {...(button.link && {
+                LinkComponent: Link,
+                to: `/${button.link}`,
+              })}>
+              {button.text}
+            </Button>
+          ))}
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default DialogComponent;
